545e1b4c6951b7a29d2f76c4749d389dc5e2b338
Urvish Lodaria <urvish.lodaria@thinkanalytics.in>
2021-03-06 13:45:46 +0530

    Modified Procfile

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+
+
+app=Flask(__name__)
+
+@app.route('/health',methods=['GET'])
+def health():
+    return "Yo I am running!!!"
+
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")
